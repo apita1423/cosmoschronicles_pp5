0d00ee0059605446fd56452f8b3ca9835bc1421b
Amanda <apita1423@outlook.com>
2024-05-20 08:14:03 +0100

    Add errors to register form

--- src/pages/auth/RegisterForm.js
+++ src/pages/auth/RegisterForm.js
@@ -1,38 +1,85 @@
 
-import React from "react";
-import { Link } from "react-router-dom";
-
+import React, {useState} from "react";
+import { Link, useHistory } from "react-router-dom";
 import styles from "../../styles/LogInRegisterForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
-
-import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+import { Form, Button, Image, Col, Row, Container, Alert } from "react-bootstrap";
+import axios from "axios";
 
 const RegisterForm = () => {
+	const [registerData, setRegisterData] = useState({
+		username: "",
+		password1: "",
+		password2: "",
+	});
+
+	const { username, password1, password2 } = registerData;
+
+	const [errors, setErrors] = useState({});
+
+	const history = useHistory();
+
+	const handleChange = (event) => {
+		setRegisterData({
+			...registerData,
+			[event.target.name]: event.target.value,
+		});
+	};
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		try {
+			await axios.post("/dj-rest-auth/registration/", registerData);
+			history.push("/login");
+		} catch (err) {
+			setErrors(err.response?.data);
+		}
+	};
+
     return (
       <Row className={styles.Row}>
         <Col className="my-auto py-2 p-md-2" md={6}>
           <Container className={`${appStyles.Content} p-4 `}>
             <h1 className={styles.Header}>Register</h1>
 
-            <Form>
+            <Form onSubmit={handleSubmit}>
               <Form.Group controlId="username">
                 <Form.Label className="d-none">Username</Form.Label>
-                <Form.Control className={styles.Input} type="text" placeholder="Username" name="username" />
+                <Form.Control className={styles.Input} type="text" placeholder="Username" name="username" value={username} onChange={handleChange} />
               </Form.Group>
+              {errors.username?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                  {message}
+                </Alert>
+              ))}
 
               <Form.Group controlId="password1">
                 <Form.Label className="d-none">Password</Form.Label>
-                <Form.Control className={styles.Input} type="password" placeholder="Password" name="password1" />
+                <Form.Control className={styles.Input} type="password" placeholder="Password" name="password1" value={password1} onChange={handleChange} />
               </Form.Group>
+              {errors.password1?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                  {message}
+                </Alert>
+              ))}
 
               <Form.Group controlId="password2">
                 <Form.Label className="d-none">Confirm Password</Form.Label>
-                <Form.Control className={styles.Input} type="password" placeholder="Confirm Password" name="password2" />
+                <Form.Control className={styles.Input} type="password" placeholder="Confirm Password" name="password2" value={password2} onChange={handleChange} />
               </Form.Group>
+              {errors.password2?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                  {message}
+                </Alert>
+              ))}
+
               <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`} type="submit">
                 Register
               </Button>
+				  {errors.non_field_errors?.map((message, idx) => (
+					<Alert key={idx} variant="warning" className="mt-3">{message}</Alert>
+				  ))}
             </Form>
           </Container>
           <Container className={`mt-3 ${appStyles.Content}`}>
@@ -45,7 +92,7 @@ const RegisterForm = () => {
           <Image className={`${appStyles.FillerImage}`} src={"https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero2.jpg"} />
         </Col>
       </Row>
-    );
+   );
 };
 
 export default RegisterForm;
