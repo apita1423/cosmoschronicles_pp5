b399a7dce05fe838c31306caa0e54e81b19e2405
Amanda <apita1423@outlook.com>
2024-05-23 16:37:14 +0100

    Add axiosRes and axiosReq to CurrentUserContext.js

--- src/api/axiosDefault.js
+++ src/api/axiosDefault.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://cc-drf-api-pp5-b19f7ab60297.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
