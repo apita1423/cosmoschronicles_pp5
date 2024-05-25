62e2a9b9945bd3f28ff8eaea8d215c0cc8a0602a
Amanda <apita1423@outlook.com>
2024-05-25 17:26:45 +0100

    Add search bar and functionality to search various posts

--- src/App.js
+++ src/App.js
@@ -7,14 +7,21 @@ import RegisterForm from "./pages/auth/RegisterForm";
 import LogInForm from "./pages/auth/LogInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
+import PostsPage from "./pages/posts/PostsPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route exact path="/" render={() => <PostsPage message="No results found. Adjust the search keyword." />} />
+          <Route exact path="/feed" render={() => <PostsPage message="No results found. Adjust the search keyword or follow a user." filter={`owner__followed__owner__profile=${profile_id}&`} />} />
+          <Route exact path="/liked" render={() => <PostsPage message="No results found. Adjust the search keyword or like a post." filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`} />} />
           <Route exact path="/login" render={() => <LogInForm />} />
           <Route exact path="/register" render={() => <RegisterForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
