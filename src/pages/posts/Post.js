b0518f8ad8d1283364fd0e7c5c9238b3dfc9dc37
Amanda <apita1423@outlook.com>
2024-05-27 16:51:52 +0100

    Add a dropdown for editing and deleting posts

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -2,9 +2,11 @@ import React from 'react';
 import styles from "../../styles/Post.module.css";
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import { Card, Media, OverlayTrigger, Tooltip } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, ueHistory } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import { axiosRes } from '../../api/axiosDefault';
+import { MoreDropdown } from '../../components/MoreDropdown';
+import { useHistory } from 'react-router';
 
 const Post = (props) => {
     const {
@@ -25,6 +27,20 @@ const Post = (props) => {
 
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner
+    const history = useHistory();
+
+    const handleEdit = () => {
+        history.push(`/posts/${id}/edit`);
+    };
+
+    const handleDelete = async () => {
+        try {
+            await axiosRes.delete(`/posts/${id}/`);
+            history.goBack();
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
     const handleLike = async () => {
         try {
@@ -58,8 +74,6 @@ const Post = (props) => {
         }
     };
 
-
-
     return <Card className={styles.Post}>
         <Card.Body>
             <Media className="align-items-center justify-content-between">
@@ -69,7 +83,7 @@ const Post = (props) => {
                 </Link>
                 <div className="d-flex align-items-center">
                     <span>{updated_at}</span>
-                    {is_owner && postPage && "..."}
+                    {is_owner && postPage && <MoreDropdown handleEdit={handleEdit} handleDelete={handleDelete} />}
                 </div>
             </Media>
         </Card.Body>
