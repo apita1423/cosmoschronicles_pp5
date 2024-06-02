9f55045ab7d7a10577544b48cc3b82125e1e0d42
Amanda <apita1423@outlook.com>
2024-06-02 18:30:27 +0100

    Add tokem timestamps

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,4 @@
+import jwtDecode from "jwt-decode";
 import { axiosReq } from "../api/axiosDefault";
 
 export const fetchMoreData = async (resource, setResource) => {
@@ -25,14 +26,27 @@ export const followHelper = (profile, clickedProfile, following_id) => {
     : profile;
 };
 
-  export const unfollowHelper = (profile, clickedProfile) => {
-    return profile.id === clickedProfile.id
-      ? {
-          ...profile,
-          followers_count: profile.followers_count - 1,
-          following_id: null,
-        }
-      : profile.is_owner
-      ? { ...profile, following_count: profile.following_count - 1 }
-      : profile;
-  };
+export const unfollowHelper = (profile, clickedProfile) => {
+  return profile.id === clickedProfile.id
+    ? {
+        ...profile,
+        followers_count: profile.followers_count - 1,
+        following_id: null,
+      }
+    : profile.is_owner
+    ? { ...profile, following_count: profile.following_count - 1 }
+    : profile;
+};
+
+export const setTokenTimestamp = (data) => {
+  const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp;
+  localStorage.setItem("refreshTokenTimestamp", refreshTokenTimestamp);
+};
+
+export const shouldRefreshToken = () => {
+  return !!localStorage.getItem("refreshTokenTimestap");
+};
+
+export const removeTokenTimestamp = () => {
+  localStorage.removeItem("refeshTokenTimestamp");
+};
