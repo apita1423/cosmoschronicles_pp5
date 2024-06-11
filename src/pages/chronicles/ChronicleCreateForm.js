dcb7e9d69a5703d877ac4e1246de8bf63a4671a3
Amanda <apita1423@outlook.com>
2024-06-11 14:36:00 +0100

    Change the font to Reddit Mono

--- src/pages/chronicles/ChronicleCreateForm.js
+++ src/pages/chronicles/ChronicleCreateForm.js
@@ -154,6 +154,8 @@ function ChronicleCreateForm() {
             <Row>
                 <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
                     <Container className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}>
+                        <h1 className="text-center">Create News</h1>
+                        <br />
                         <Form.Group className="text-center" aria-label="image">
                             {image ? (
                                 <>
--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -99,6 +99,8 @@ function PostCreateForm() {
             <Row>
                 <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
                     <Container className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}>
+                        <h1 className="text-center">Create Post</h1>
+                        <br />  
                         <Form.Group className="text-center">
                             {image ? (
                                 <>
